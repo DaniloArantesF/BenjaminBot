65a5345423b6062c87d04521782cdee576a23f10
Danilo Arantes <dfilho@purdue.edu>
2020-07-07 13:12:26 -0400

    no longer wait for all requests

--- spotify.js
+++ spotify.js
@@ -13,7 +13,6 @@ const youtube = API;
  * https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
  * Using url-encoded http requests: https://github.com/axios/axios#using-applicationx-www-form-urlencoded-format
  */
-
 var encoded_credentials = Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_SECRET).toString('base64');
 var spotifyToken = '';
 
@@ -37,35 +36,34 @@ async function handleRequest(message, uri, serverQueue) {
     var songs = await getSpotifyPlaylist(message, playlist_Id, serverQueue);
 
     if(!serverQueue) {
-        const newQueue = {
-          textChannel: message.channel,
-          voiceChannel: voiceChannel,
-          connection: null,
-          songs: songs,
-          volume: 5,
-          playing: true,
-          playingEmbed: null,
-          queueEmbed: null
-        };
-  
-        /* Set new queue */
-        queue.set(message.guild.id, newQueue);
+      const newQueue = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: songs,
+        volume: 5,
+        playing: true,
+        playingEmbed: null,
+        queueEmbed: null
+      };
 
-        /* Join Voice Channel */
-        voiceChannel.join().then(connection => {
-          newQueue.connection = connection;
-          play(message.guild, newQueue.songs[0]);
-          message.delete();
-        });
+      /* Set new queue */
+      queue.set(message.guild.id, newQueue);
 
-      } else {
-        for (song in songs) {
-          serverQueue.songs.push(songs[song]);
-        }
-
-        message.channel.send(`Playlist adicionada com sucesso, caralho!`);
+      /* Join Voice Channel */
+      voiceChannel.join().then(connection => {
+        newQueue.connection = connection;
+        play(message.guild, newQueue.songs[0]);
         message.delete();
+      });
+
+    } else {
+      for (song in songs) {
+        serverQueue.songs.push(songs[song]);
       }
+      //message.channel.send('Playlist adicionada com sucesso, caralho!');
+      message.delete();
+    }
 }
 
 /* Returns an array w/ all song names and urls in specified playlist */
@@ -79,17 +77,15 @@ async function getSpotifyPlaylist(message, playlist_Id, serverQueue) {
       }})
       .then(async response => {
         var data = response.data.items;
-        var songs = [];
+        var songs = serverQueue || [];
         var songNames = [];
 
         for (song in data) {
-          songNames[song] = data[song].track.artists[0].name + " " + data[song].track.name;
+          songNames[song] = data[song].track.name + ", " + data[song].track.artists[0].name;
         }
 
-        console.log(songNames);
-
         var searchQueue = async.queue((song, callback) => {
-          //console.log("Processing song... " + song.name);
+          //console.log("\nProcessing " + song.name + "...");
           youtube.search.list({part:'snippet', q: song.name, maxResults: 1, safeSearch:'none', type:'video', regionCode:'US', videoCategoryId:'10' })
             .then( (response) => {
               callback(song.name, response);
@@ -97,22 +93,30 @@ async function getSpotifyPlaylist(message, playlist_Id, serverQueue) {
         }, 1);
 
         for (song in songNames) {
-          //console.log("Adding to Queue... " + songNames[song]);
+          //console.log("Pushing " + songNames[song] + "...");
           searchQueue.push({name: songNames[song]}, (songName, response) => {
-            console.log("Callback for: " + songName);
             var url = "https://www.youtube.com/watch?v=" + response.data.items[0].id.videoId;
             var song_info = {
               title: songName,
               url: url,          
             };
-            //console.log(song_info.title + " " + song_info.url);
+            //console.log("\nFinished processing " + songName + "...");
             songs.push(song_info);
-            //console.log(songs.toString()); 
           });
         }
-
-        await searchQueue.drain();
-        return songs
+        const isReady = async () => {
+          if (searchQueue.length() < songNames.length) {
+            //console.log("ready")
+            return true;
+          } else {
+            //console.log("not ready");
+            setTimeout(isReady, 100);
+          }
+        }
+        return new Promise(async resolve => {
+          await isReady();
+          resolve(songs);
+        });
       })
       .catch(error => {
         console.log(error.message);
